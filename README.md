42ea5a24d26019501948b4bcf99af4e140170320
Eric Ung <ungericw@tutanota.com>
2023-10-16 13:30:00 -0500

    dost

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# dost
+# Dev Ops Speech to Text
+
+The Dev Ops Speech to Text, dost, project aims to execute cloud commands from voice.
--- dost/Program.cs
+++ dost/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Speech.Recognition;
+using System.Threading;
+
+namespace SharedRecognizer
+{
+    [System.Runtime.Versioning.SupportedOSPlatform("windows")]
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Initialize an instance of the shared recognizer.  
+            using (SpeechRecognizer recognizer = new SpeechRecognizer())
+            {
+                // Create and load a sample grammar.  
+                Grammar testGrammar = CreateColorGrammar();
+                testGrammar.Name = "Test Grammar";
+                recognizer.LoadGrammar(testGrammar);
+
+                // Attach event handlers for recognition events.  
+                recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(SpeechRecognizedHandler);
+
+                Console.ReadLine();
+
+            }
+        }
+
+        public static Grammar CreateColorGrammar()
+        {
+            // Create a set of color choices.  
+            Choices azureGroupChoices = new Choices(new string[] { "devops", "pipelines", "boards", "repos", "artificacts" });
+            GrammarBuilder colorElement = new GrammarBuilder(azureGroupChoices);
+
+            // Create grammar builders for the two versions of the phrase.  
+            GrammarBuilder azureCommand = new GrammarBuilder(new Choices(new string[] { "azure", "a z"}));
+            azureCommand.Append(azureGroupChoices);
+            GrammarBuilder quitPhrase = new GrammarBuilder("Quit");
+
+            // Create a Choices for the two alternative phrases, convert the Choices  
+            // to a GrammarBuilder, and construct the grammar from the result.  
+            Choices bothChoices = new Choices(new GrammarBuilder[] { azureCommand, quitPhrase });
+            Grammar grammar = new Grammar((GrammarBuilder)bothChoices);
+            grammar.Name = "Commands";
+            return grammar;
+        }
+
+        // Handle the SpeechRecognized event.  
+        public static void SpeechRecognizedHandler(object? sender, SpeechRecognizedEventArgs e)
+        {
+            if (e.Result != null)
+            {
+                if ()
+                Console.WriteLine("Recognition result = {0}", e.Result.Text ?? "<no text>");
+
+                if (e.Result.Text == "Quit")
+                {
+                }
+            }
+            else
+            {
+                Console.WriteLine("No recognition result");
+            }
+        }
+    }
+}
