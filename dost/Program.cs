f48fe229df7fdc90bea085d320d118189fedacfd
Eric Ung <ungericw@tutanota.com>
2023-10-16 14:05:55 -0500

    az costmanagement

--- dost/Program.cs
+++ dost/Program.cs
@@ -25,15 +25,23 @@ namespace SharedRecognizer
 
         public static Grammar CreateColorGrammar()
         {
+            // az devops
             Choices azureGroupChoices = new Choices(new string[] { "devops", "pipelines", "boards", "repos", "artificacts" });
             Choices azureSubgroupsChoices = new Choices(new string[] { "admin", "extension", "project", "security", "service-endpoint", "team", "user", "wiki"});
             Choices azureCommandsChoices = new Choices(new string[] { "configure", "feedback", "invoke", "login", "logout" });
 
+            // az costmanagement
+            Choices azureCostManagementChoices = new Choices(new string[] { "costmanagement" });
+            Choices azureCostManagementExportChoices = new Choices(new string[] { "export" });
+
             GrammarBuilder azureGroupGrammar = new GrammarBuilder(azureGroupChoices);
             azureGroupGrammar.Append(azureSubgroupsChoices);
+            GrammarBuilder azureCostManagementGrammarBuilder = new GrammarBuilder(azureCostManagementChoices);
+            azureCostManagementGrammarBuilder.Append(azureCostManagementExportChoices);
             GrammarBuilder azureCommand = new GrammarBuilder(new Choices(new string[] { "azure", "a z"}));
-            azureCommand.Append(azureGroupChoices);
+            azureCommand.Append(azureGroupGrammar);
             azureCommand.Append(azureCommandsChoices);
+            azureCommand.Append(azureCostManagementGrammarBuilder);
             GrammarBuilder quitPhrase = new GrammarBuilder("Quit");
 
             Choices bothChoices = new Choices(new GrammarBuilder[] { azureCommand, quitPhrase });
